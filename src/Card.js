a926050cd45418c3c32dc5b8a48ff74777766776
Samira77-ss <samiraseyfii0709@gmail.com>
2023-11-03 14:16:22 +0330

    Added HTML and CSS initial structure

--- src/Card.js
+++ src/Card.js
@@ -6,27 +6,27 @@ function Card() {
   return (
     <div className="container">
       <div className="content-section">
-        <div className="card">
-          <img src={iconSedans} alt="icon" width={50} />
-          <h1>Sedans</h1>
+        <div className="card sedans">
+          <img src={iconSedans} alt="icon" />
+          <h2>Sedans</h2>
           <p>
             Choose a sedan for its affordability and excellent fuel economy.
             Ideal for cruising in the city or on your next road trip.
           </p>
           <button className="btn btn primary">Learn More</button>
         </div>
-        <div className="card">
-          <img src={iconSuvs} alt="icon" width={50} />
-          <h1>SUVS</h1>
+        <div className="card suvs">
+          <img src={iconSuvs} alt="icon" />
+          <h2>SUVS</h2>
           <p>
             Take an SUV for its spacious interior, power, and versatility.
             Perfect for your next family vacation and off-road adventures.
           </p>
           <button className="btn btn primary">Learn More</button>
         </div>
-        <div className="card">
-          <img src={iconLuxury} alt="icon" width={50} />
-          <h1>LUXURY</h1>
+        <div className="card luxury">
+          <img src={iconLuxury} alt="icon" />
+          <h2>LUXURY</h2>
           <p>
             Cruise in the best car brands without the bloated prices. Enjoy the
             enhanced comfort of a luxury rental and arrive in style.
