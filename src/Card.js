6d1b77cd00b796a71b9b56b99c563d8bb9186f9a
Samira77-ss <samiraseyfii0709@gmail.com>
2023-11-05 10:47:49 +0330

    Added HTML and CSS structures to build the page

--- src/Card.js
+++ src/Card.js
@@ -4,35 +4,42 @@ import iconSedans from "./image/icon-sedans.svg";
 import iconSuvs from "./image/icon-suvs.svg";
 function Card() {
   return (
-    <div className="container">
-      <div className="content-section">
-        <div className="card sedans">
+    <div className="card">
+      <div className="sedans">
+        <div className="content">
+          {" "}
           <img src={iconSedans} alt="icon" />
-          <h2>Sedans</h2>
+          <h2>SEDANS</h2>
           <p>
             Choose a sedan for its affordability and excellent fuel economy.
             Ideal for cruising in the city or on your next road trip.
           </p>
-          <button className="btn btn primary">Learn More</button>
         </div>
-        <div className="card suvs">
+        <button className="firstButton">Learn More</button>
+      </div>
+      <div className="suvs">
+        <div className="content">
+          {" "}
           <img src={iconSuvs} alt="icon" />
           <h2>SUVS</h2>
           <p>
             Take an SUV for its spacious interior, power, and versatility.
             Perfect for your next family vacation and off-road adventures.
           </p>
-          <button className="btn btn primary">Learn More</button>
         </div>
-        <div className="card luxury">
+        <button className="secondButton">Learn More</button>
+      </div>
+      <div className="luxury">
+        <div className="content">
+          {" "}
           <img src={iconLuxury} alt="icon" />
           <h2>LUXURY</h2>
           <p>
             Cruise in the best car brands without the bloated prices. Enjoy the
             enhanced comfort of a luxury rental and arrive in style.
           </p>
-          <button className="btn btn primary">Learn More</button>
         </div>
+        <button className="thirdButton">Learn More</button>
       </div>
     </div>
   );
